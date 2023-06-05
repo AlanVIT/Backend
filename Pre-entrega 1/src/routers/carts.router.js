6465497dd2856d9417f83efe6d2750dd5e4ef40e
AlanVT <alanvitas27@gmail.com>
2023-06-05 15:21:03 -0300

    first commit

--- Pre-entrega 1/src/routers/carts.router.js
+++ Pre-entrega 1/src/routers/carts.router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
+import { cartManager } from "../managers/carts.js";
 
 const router = Router();
 const __filename = fileURLToPath(import.meta.url);
@@ -23,71 +24,23 @@ function readCartsFromFile() {
   }
 }
 
-function writeCartsToFile(carts) {
-  try {
-    fs.writeFileSync(CARTS_FILE, JSON.stringify(carts, null, 2));
-  } catch (err) {
-    console.error("Error writing carts file:", err);
-  }
-}
-
 let carts = readCartsFromFile();
+const cartM = new cartManager();
 
 router.get('/', (req, res) => {
-  res.send(JSON.stringify(carts));
+  cartM.getCarts(res, carts)
 });
 
 router.get('/:id', (req, res) => {
-  const cartId = parseInt(req.params.id);
-  const cart = carts.find(e => e.id === cartId);
-
-  if (!cart) {
-    return res.status(404).send({ status: 'error', message: 'Carrito no encontrado' });
-  }
-
-  res.send(JSON.stringify(cart.products));
+  cartM.getCartsId(res,req,carts)
 });
 
 router.post('/', (req, res) => {
-  const cart = req.body;
-
-  const requiredFields = ['products', 'quantity'];
-  const missingFields = requiredFields.filter(field => !cart[field]);
-
-  if (missingFields.length > 0) {
-    return res.status(400).send({ status: 'error', message: 'Faltan campos requeridos', missingFields });
-  } else {
-    cart.id = Math.round(Math.random() * 10000);
-    carts.push(cart);
-    writeCartsToFile(carts);
-    res.send({ status: "success" });
-  }
-
-  if (typeof cart.status === 'undefined') {
-    cart.status = true;
-  }
+  cartM.newCart(res,req,carts)
 });
 
 router.post('/:cid/product/:pid', (req, res) => {
-  const cartId = parseInt(req.params.cid);
-  const productId = req.params.pid;
-
-  const cart = carts.find(e => e.id === cartId);
-  if (parseFloat(cartId) != cartId || parseFloat(productId) != productId) {
-    return res.status(400).send({ status: 'error', message: 'Los IDs deben ser números' });
-  }
-  if (!cart) {
-    return res.status(404).send({ status: 'error', message: 'Carrito no encontrado' });
-  }
-
-  if (!cart.products) {
-    cart.products = [];
-  }
-
-  cart.products.push(parseInt(productId));
-
-  writeCartsToFile(carts);
-  res.send({ status: 'success' });
+  cartM.cartPrd(res,req,carts)
 });
 
 export default router;
--- Pre-entrega 1/src/routers/products.router.js
+++ Pre-entrega 1/src/routers/products.router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
+import { ProductManager } from "../managers/products.js";
 
 const router = Router();
 const __filename = fileURLToPath(import.meta.url);
@@ -20,83 +21,33 @@ function readProductsFromFile() {
   }
 }
 
-function writeProductsToFile(products) {
-  try {
-    fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2));
-  } catch (err) {
-    console.error("Error writing products file:", err);
-  }
-}
+
 
 let prds = readProductsFromFile();
 
+const productM = new ProductManager();
+
 router.get('/', (req, res) => {
-  let limit = req.query.limit ? parseInt(req.query.limit) : undefined;
-  let responseProducts = limit ? prds.slice(0, limit) : prds;
-  res.send(JSON.stringify(responseProducts));
+  productM.getPrds(res, req, prds)
 });
 
 router.get('/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = prds.find(e => e.id === productId);
 
-  if (!product) {
-    return res.status(404).send({ status: 'error', message: 'Producto no encontrado' });
-  }
+  productM.getPrdsId(res, req, prds)
 
-  res.send(JSON.stringify(product));
 });
 
 router.delete('/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = prds.find(e => e.id === productId);
-
-  if (!product) {
-    return res.status(404).send({ status: 'error', message: 'Producto no encontrado' });
-  }
-
-  const index = prds.findIndex(product => product.id === productId);
-  prds.splice(index, 1);
-  writeProductsToFile(prds);
-  res.send(JSON.stringify(index));
+  productM.deletePrds(res, req, prds)
 });
 
 router.put('/:id', (req, res) => {
-  const change = req.body;
-  const product = prds.find(product => product.id === parseInt(req.params.id));
+  productM.upgradePrd(res, req, prds)
 
-  if (!product) {
-    return res.status(404).send({ status: 'error', message: 'Producto no encontrado' });
-  }
-
-  for (let key in change) {
-    if (product.hasOwnProperty(key)) {
-      product[key] = change[key];
-    }
-  }
-
-  writeProductsToFile(prds);
-  res.send({ status: 'success', product: product });
 });
 
 router.post('/', (req, res) => {
-  const prd = req.body;
-
-  const requiredFields = ['title', 'description', 'code', 'price', 'stock', 'category'];
-  const missingFields = requiredFields.filter(field => !prd[field]);
-
-  if (missingFields.length > 0) {
-    return res.status(400).send({ status: 'error', message: 'Faltan campos requeridos', missingFields });
-  } else {
-    prd.id = Math.round(Math.random() * 10000);
-    prds.push(prd);
-    writeProductsToFile(prds);
-    res.send({ status: "success" });
-  }
-
-  if (typeof prd.status === 'undefined') {
-    prd.status = true;
-  }
+  productM.newPrd(res, req, prds)
 });
 
 export default router;
