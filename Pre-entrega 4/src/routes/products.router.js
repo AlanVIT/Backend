a15d561c221a2e4b3f16c3491336ebe2f2697273
AlanVT <alanvitas27@gmail.com>
2023-08-24 16:03:09 -0300

    Mocking, errores y esas cosas

---
+++ Pre-entrega 4/src/routes/products.router.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { mockingproducts,getProducts,addProduct,getProductById,deleteProduct,updateProduct } from "../controllers/products.controller.js";
+
+const router = Router();
+
+router.get('/',getProducts);
+
+router.get('/:productId', getProductById);
+
+router.post('/', addProduct);
+
+router.put('/:productId',updateProduct);
+
+router.delete('/:productId',deleteProduct);
+
+router.get('/mockingproducts', mockingproducts)
+
+
+export default router;
